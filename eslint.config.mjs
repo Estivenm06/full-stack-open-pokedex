5daaf8114701fdb4a5177f0be7192758f1c07568
LUIGIWONKA <mejiaguetoestiven@gmail.com>
2024-11-19 12:51:32 -0500

    Adding playwright test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,30 @@
-import globals from 'globals'
-import js from '@eslint/js'
-import stylisticJs from '@stylistic/eslint-plugin-js'
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['**/dist/*']
+    ignores: ["**/dist/*", "**/test-results/*", "**/e2e-tests/*", "**/playwright-report/*"],
   },
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
-      sourceType: 'module',
+      sourceType: "module",
       globals: {
         ...globals.node,
       },
-      ecmaVersion: 'latest',
+      ecmaVersion: "latest",
     },
     plugins: {
-      '@stylistic/js': stylisticJs
+      "@stylistic/js": stylisticJs,
     },
     rules: {
-      '@stylistic/js/indent': [
-        'error',
-        2
-      ],
-      '@stylistic/js/linebreak-style': [
-        'error',
-        'unix'
-      ],
-      '@stylistic/js/quotes': [
-        'error',
-        'single'
-      ],
-      '@stylistic/js/semi': [
-        'error',
-        'never'
-      ],
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "unix"],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/semi": ["error", "never"],
     },
   },
-]
+];
