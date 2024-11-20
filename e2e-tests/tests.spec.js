084b489922bf79494e5e3a797d4b54af11b678f8
LUIGIWONKA <mejiaguetoestiven@gmail.com>
2024-11-19 23:39:09 -0500

    e2e test for individual pokemon

--- e2e-tests/tests.spec.js
+++ e2e-tests/tests.spec.js
@@ -12,4 +12,9 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+  test('pokemon page can be navigated to', async ({page}) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
